bdae3b270bfb817e14fefe264a9b07a0efb4851f
MOULOUD EL ARRAM <maoiloud2002@gmail.com>
2023-11-07 16:14:41 +0100

    add index file

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,6 @@ import { HelloMessage } from './HelloMessage';
 @Injectable()
 export class AppService {
   getHello(name: string): HelloMessage {
-    return { hello: `Hello, ${name}!` };
+    return { hello: `Hello, ${name}!`/*"everyone" */};
   }
 }
